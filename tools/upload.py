3ca276d0f0ebcfea95c14e59acb6b2884002abbf
Barosl LEE <vcs@barosl.com>
2013-06-01 06:44:05 +0900

    Improve implementation

--- tools/upload.py
+++ tools/upload.py
@@ -4,22 +4,14 @@ import flickr_api
 import tempfile
 import convert
 import os
+import utils
 
 def main():
-	def login():
-		auth = flickr_api.auth.AuthHandler()
-		url = auth.get_authorization_url('write')
-		key = raw_input('Visit\n%s\n...and type the oauth_verifier code: ' % url)
-		auth.set_verifier(key)
-		auth.save('auth.txt')
-		return auth
-
-	try: flickr_api.set_auth_handler('auth.txt')
-	except IOError: flickr_api.set_auth_handler(login())
+	utils.flickr_login()
 
 #	flickr_api.upload(photo_file='in.jpg', title='Test twice')
 
-	img_data = convert.encode_to_png(open('in.mp3', 'rb').read())
+	img_data = convert.encode_to_png(open('in.mp3', 'rb').read(), font='in.ttf')
 
 	try:
 		fp = tempfile.NamedTemporaryFile(delete=False)
